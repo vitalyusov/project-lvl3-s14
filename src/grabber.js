de883446cc5dc2786434ee240dd08b6ade89da66
Vitaly Usov <v.usov@ctm.ru>
2017-02-15 18:16:53 +0300

    grabber changes

--- src/grabber.js
+++ src/grabber.js
@@ -2,7 +2,14 @@ import cheerio from 'cheerio';
 
 export default (dir = '.', data) => {
   const $ = cheerio.load(data);
-  console.log($('link, src'));
+  const links = $('link');
+  const scripts = $('script').filter((i, el) => $(el).attr('src'));
+  const imgs = $('img');
+  const items = [...links, ...scripts, ...imgs];
+  //console.log(links[2].attribs.href);
+  console.log(scripts[0].attribs.src);
+  //console.log(imgs);
+  //console.log([...links, ...scripts, ...imgs]);
   return new Promise((resolve, reject) => {
     resolve(data);
   });
